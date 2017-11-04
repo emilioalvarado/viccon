30b0c861b1be52992bab30d6debae5d5b23d8d41
Emilio Alvarado <emilio.alvarado@gmail.com>
2017-11-04 16:11:21 -0500

    Actualizacion datos de configuración
    Actualizacion datos de configuración

--- Backend/config.js
+++ Backend/config.js
@@ -16,5 +16,5 @@ module.exports = {
     nombreBucket:'s3.viccon.bucket1',
     rutaMultimediaCron: '/home/ec2-user/viccon/public/media/',
     QueueUrl: 'https://sqs.us-east-2.amazonaws.com/911317221798/sqs-viccon-video-standard',
-    QueueTiempoProcesamiento: '40' //40 segundos
+    QueueTiempoProcesamiento: '40' //segundos
 }
